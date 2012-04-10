9eee73184924d4539b5cc24d427f3dabdaedafd9
Alberto <alberto@fatbeehive.com>
2012-04-10 11:33:19 +0100

    loop all .dotfiles to link in home folder

--- install.sh
+++ install.sh
@@ -4,41 +4,20 @@
 
 echo "-= Install dotfiles. =-"
 
-DOTFILES="$HOME/dotfiles"
+DOTFILES="$HOME/.dotfiles"
 
 export DOTFILES
 
 echo "\n-= Clone dotfiles repository. =-"
-git clone git@github.com:dsjbirch/dotfiles.git $DOTFILES
-
-echo "\n-= Create directory structure. =-"
-mkdir -p $HOME/etc
+git clone git@github.com:albertolopez/dotfiles.git $DOTFILES
 
 echo "\n-= Create symlinks. =-"
-link 'bash-dotfiles/bash_logout' '.bash_logout'
-link 'bash-dotfiles/bash_profile' '.bash_profile'
-link 'bash-dotfiles/bashrc' '.bashrc'
-link 'bash-dotfiles/inputrc' '.inputrc'
-
-link 'rails-templates' 'etc/rails-templates'
-
-link 'aliases' '.aliases'
-link 'environ' '.environ'
-link 'functions' '.functions'
-link 'profile' '.profile'
-link 'screenrc' '.screenrc'
+for f in $DOTFILES
+do
+link "$DOTFILES/$f" "$HOME/$f"
+done
 
 echo "\n-= Update $DOTFILES submodules. =-"
 cd $DOTFILES
 git submodule init
 git submodule update
-
-# # GIT
-# link_repo_file_to_home_file 'git/gitconfig' '.gitconfig'
-# 
-# # SHELL
-# link_repo_file_to_home_file "shell/profile" ".profile"
-# 
-# # BASH 
-# link_repo_file_to_home_file 'shell/bash/bashrc' '.bashrc'
-# link_repo_file_to_home_file 'shell/bash/bash_profile' '.bash_profile'
